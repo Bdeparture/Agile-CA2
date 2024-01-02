dca4395b84203cf3bcddb3fa3aaa6c3f6d31d907
Sisi Chen <1599668078@qq.com>
2024-01-02 12:11:55 +0800

    add the genres test

--- api/genres/index.js
+++ api/genres/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { genres } from './genres';
+import Genre from './genreModel';
+import { getGenres } from '../tmdb-api';
 
 const router = express.Router(); 
 
-router.get('/', (req, res) => {
-    res.json(genres);
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
 
+router.get('/tmdb', async (req, res) => {
+    const genres = await getGenres();
+    res.status(200).json(genres);
+});
 export default router;
